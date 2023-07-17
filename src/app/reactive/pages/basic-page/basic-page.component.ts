278e5841708bc02a20e60df3d66544789ea5fd08
Jesus Marenco <jesusmarencodev@gmail.com>
2023-07-17 06:51:20 -0500

    html forms

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basic-page.component.html',
+  styleUrls: ['./basic-page.component.scss']
+})
+export class BasicPageComponent {
+
+  // public myForm: FormGroup = new FormGroup
+}
